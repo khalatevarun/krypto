234b0d37b9fbe15d8863d17d8207e64374ce7d8d
khalatevarun <khalatevarun@gmail.com>
2026-01-17 23:54:32 -0600

    setup basic chat completion non streaming response feature

---
+++ client/llm_client.py
@@ -0,0 +1,42 @@
+from typing import Any
+from openai import AsyncOpenAI
+
+class LLMClient:
+    def __init__(self) -> None:
+        self._client: AsyncOpenAI | None = None
+    
+    def get_client(self) -> AsyncOpenAI:
+        if self._client is None:
+            self._client = AsyncOpenAI(
+                api_key='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',
+                base_url='https://openrouter.ai/api/v1'
+            )
+        return self._client
+
+    async def close(self) -> None:
+        if self._client:
+            await self._client.close()
+            self._client = None
+
+    async def chat_completion(self, messages: list[dict[str, Any]], stream: bool = True):
+        client = self.get_client()
+
+        kwargs = {
+            "model": "mistralai/devstral-2512:free",
+            "messages": messages,
+            "stream": stream
+        }
+
+
+        if stream:
+            await self._stream_response()
+        else:
+           await self._non_stream_response(client, kwargs)
+
+    async def _stream_response(self):
+        pass
+
+    async def _non_stream_response(self, client: AsyncOpenAI, kwargs: dict[str, Any]):
+        response = await client.chat.completions.create(**kwargs) 
+        ## ** mean deconstructing the args 
+        print(response)
---
+++ main.py
@@ -0,0 +1,14 @@
+from client.llm_client import LLMClient
+import asyncio
+
+
+async def main():
+    client = LLMClient()
+    messages = [{
+        'role':'user',
+        'content': 'what is up?'
+    }]
+    await client.chat_completion(messages, False)
+    print("done")
+
+asyncio.run(main())
